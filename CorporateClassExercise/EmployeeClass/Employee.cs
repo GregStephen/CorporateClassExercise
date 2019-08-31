f6fc52a623fdddd56b19ed0114d78cd58e8e63a4
Greg Stephen <GregAStephen@gmail.com>
2019-08-31 11:07:25 -0500

    first commit

---
+++ CorporateClassExercise/EmployeeClass/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorporateClassExercise.EmployeeClass
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime StartDate { get; }
+        public Employee(string name, string jobTitle)
+        {
+            Name = name;
+            JobTitle = jobTitle;
+            StartDate = DateTime.Now;
+        }
+    }
+}
---
+++ CorporateClassExercise/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using CorporateClassExercise.EmployeeClass;
+
+namespace CorporateClassExercise
+{
+    class Program
+    {
+        public class Company
+        {
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+
+            List<Employee> _currentEmployees = new List<Employee>();
+
+            public void AddEmployee(Employee employee)
+            {
+                _currentEmployees.Add(employee);
+            }
+
+            public void FireEmployee(Employee employee)
+            {
+                _currentEmployees.Remove(employee);
+            }
+
+            public void ListEmployees()
+            {
+                foreach (Employee employee in _currentEmployees )
+                {
+                    Console.WriteLine($"{employee.Name} is a {employee.JobTitle} at {Name}");
+                }
+            }
+            public Company(string name)
+            {
+                Name = name;
+                CreatedOn = DateTime.Now;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var company = new Company("PoopScoop Incorporated");
+            var employee1 = new Employee("John", "Data Analyst");
+            var employee2 = new Employee("Janice", "Boss of All");
+            var employee3 = new Employee("Jeff", "Coffee Bitch");
+            company.AddEmployee(employee1);
+            company.AddEmployee(employee2);
+            company.AddEmployee(employee3);
+            company.ListEmployees();
+
+        }
+    }
+}
